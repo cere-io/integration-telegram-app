eb8c1726b8f5050e6cc393c1a21e49623819b90d
Sergey Kambalin <sergey.kambalin@cere.io>
2024-08-16 16:44:02 +0600

    feature: Plan subscription

--- src/hooks/useWalletSubscription.ts
+++ src/hooks/useWalletSubscription.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { Subscription } from '@tg-app/api';
 
 import { useBot } from './useBot';
@@ -6,6 +6,15 @@ import { useBot } from './useBot';
 export const useWalletSubscriptions = (address?: string) => {
   const bot = useBot();
   const [data, setData] = useState<Subscription>();
+
+  const sync = useCallback(async () => {
+    if (!address) {
+      return;
+    }
+
+    await bot.saveSubscription(address);
+    await bot.getUserSubscription(address).then(setData);
+  }, [bot, address]);
 
   useEffect(() => {
     if (!address) {
@@ -19,6 +28,7 @@ export const useWalletSubscriptions = (address?: string) => {
 
   return {
     data,
+    sync,
     loading: data === undefined,
   };
 };
