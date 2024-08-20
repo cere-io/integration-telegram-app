f18ac1d96cb6de001eb8d318c119d96e70cd3bd4
Sergey Kambalin <sergey.kambalin@cere.io>
2024-08-20 12:57:39 +0600

    fix: Make Media SDK player work in Safari and iOS

--- src/components/VideoPlayer/VideoPlayer.tsx
+++ src/components/VideoPlayer/VideoPlayer.tsx
@@ -1,15 +1,9 @@
 import { Card, Modal, ModalProps } from '@tg-app/ui';
 import { Video } from '@tg-app/api';
-// import { IosVideoPlayer as CerePlayer } from '@cere/media-sdk-react';
+import { VideoPlayer as CerePlayer } from '@cere/media-sdk-react';
 import { useViewport } from '@telegram-apps/sdk-react';
 
-/**
-* Import the styles for the VideoPlayer component from the media-sdk-react package.
-* This is the CSS file that will be used to style the VideoPlayer component.
-
-* TODO: Properly import by package name - not by file path.
-*/
-import '../../../node_modules/@cere/media-sdk-react/dist/browser.css';
+import './VideoPlayer.css';
 
 export type VideoPlayerProps = Pick<ModalProps, 'open'> & {
   video?: Video;
@@ -41,21 +35,18 @@ export const VideoPlayer = ({ token, video, open = false, onClose }: VideoPlayer
     <Modal open={open && !!video && !!token} onOpenChange={(open) => !open && onClose?.()}>
       <Modal.Header>{video?.name}</Modal.Header>
 
-      <Card style={{ borderRadius: 0 }}>
+      <Card className="VideoPlayer-card">
         {url && (
-          <video autoPlay controls height={height} width={width}>
-            <source src={url!} type={video?.mimeType} />
-          </video>
-
-          // <CerePlayer
-          //   hlsEnabled={false}
-          //   src={url!}
-          //   type={video?.mimeType}
-          //   loadingComponent={<div />}
-          //   videoOverrides={{
-          //     autoPlay: true,
-          //   }}
-          // />
+          <CerePlayer
+            hlsEnabled={false}
+            src={url!}
+            type={video?.mimeType}
+            loadingComponent={<div />}
+            videoOverrides={{
+              autoPlay: true,
+              style: `width: ${width}px; height: ${height}px;` as any,
+            }}
+          />
         )}
 
         <Card.Cell readOnly subtitle={video?.description} style={{ paddingBottom: 16 }}>
