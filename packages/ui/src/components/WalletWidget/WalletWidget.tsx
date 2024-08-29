7955abf34d92aa3ab95c364d20651ff6ab746635
Sergey Kambalin <sergey.kambalin@cere.io>
2024-08-29 13:49:57 +0600

    Wallet header image

--- packages/ui/src/components/WalletWidget/WalletWidget.tsx
+++ packages/ui/src/components/WalletWidget/WalletWidget.tsx
@@ -1,5 +1,7 @@
-import { Banner, Button } from '@telegram-apps/telegram-ui';
+import { Title, Button, Text, Divider } from '@telegram-apps/telegram-ui';
 
+import './WalletWidget.css';
+import walletImage from './wallet.png';
 import { Truncate } from '../Truncate';
 
 export type WalletWidgetProps = {
@@ -10,21 +12,45 @@ export type WalletWidgetProps = {
 
 export const WalletWidget = ({ address, onConnect, onDisconnect }: WalletWidgetProps) => {
   return (
-    <Banner
-      header="Wallet"
-      subheader={
-        address ? <Truncate maxLength={20} variant="address" text={address} /> : 'Connect your wallet to continue'
-      }
-    >
-      {address ? (
-        <Button size="s" onClick={onDisconnect}>
-          Disconnect
-        </Button>
-      ) : (
-        <Button size="s" onClick={onConnect}>
-          Connect
-        </Button>
-      )}
-    </Banner>
+    // <Banner
+    //   header="Wallet"
+    //   subheader={
+    //     address ? <Truncate maxLength={20} variant="address" text={address} /> : 'Connect your wallet to continue'
+    //   }
+    // >
+    <>
+      <div className="WalletWidget-root">
+        <div className="WalletWidget-wallet">
+          <div className="WalletWidget-image">
+            <img src={walletImage} />
+          </div>
+
+          <Title level="2" weight="2" className="WalletWidget-title">
+            Wallet
+          </Title>
+
+          {address ? (
+            <Text className="WalletWidget-address">
+              <Truncate maxLength={20} variant="address" text={address} />
+            </Text>
+          ) : (
+            <Text className="WalletWidget-caption">Connect your wallet to continue</Text>
+          )}
+        </div>
+
+        <div className="WalletWidget-actions">
+          {address ? (
+            <Button mode="bezeled" onClick={onDisconnect}>
+              Disconnect
+            </Button>
+          ) : (
+            <Button mode="bezeled" onClick={onConnect}>
+              Connect
+            </Button>
+          )}
+        </div>
+      </div>
+      <Divider />
+    </>
   );
 };
