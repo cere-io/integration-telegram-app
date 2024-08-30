24b415608b54067fe8dfc946b37dd56512e3e640
Sergey Kambalin <sergey.kambalin@cere.io>
2024-08-30 14:59:13 +0600

    feature: Pass channel context to the bot

--- src/hooks/useBot.ts
+++ src/hooks/useBot.ts
@@ -1,10 +1,13 @@
 import { useMemo } from 'react';
+import { useInitData } from '@telegram-apps/sdk-react';
 import { BotApi } from '@tg-app/api';
 
 import { TELEGRAM_BOT_URL } from '~/constants';
 
 export const useBot = () => {
-  const botApi = useMemo(() => new BotApi(TELEGRAM_BOT_URL), []);
+  const { startParam } = useInitData() || {};
+
+  const botApi = useMemo(() => new BotApi(TELEGRAM_BOT_URL, { startParam }), [startParam]);
 
   return botApi;
 };
