1360330ea7ac5b767a1b96d1f7cda6c7f4e16ed5
upalinski <ulad@cere.io>
2024-12-01 00:23:53 +0300

    integrate cere wallet

--- src/constants.ts
+++ src/constants.ts
@@ -6,4 +6,5 @@ export const APP_URL = import.meta.env.VITE_APP_URL;
 export const TONCONNECT_MANIFEST_URL = new URL('/tonconnect-manifest.json', APP_URL).href;
 export const TELEGRAM_APP_URL = import.meta.env.VITE_TELEGRAM_APP_URL;
 export const TELEGRAM_BOT_URL = import.meta.env.VITE_TELEGRAM_BOT_URL;
+export const TELEGRAM_BOT_ID = import.meta.env.VITE_TELEGRAM_BOT_ID;
 export const DEFAULT_START_PARAM = import.meta.env.VITE_DEFAULT_START_PARAM || undefined;
