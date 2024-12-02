228ba63a959f7f270cb8e1c2d3f01881a1b8859e
Ulad Palinski <46526208+upalinski@users.noreply.github.com>
2024-12-03 02:32:58 +0400

    Integrate Cere Wallet (#35)
    Co-authored-by: upalinski <ulad@cere.io>

--- src/constants.ts
+++ src/constants.ts
@@ -6,6 +6,7 @@ export const APP_URL = import.meta.env.VITE_APP_URL;
 export const TONCONNECT_MANIFEST_URL = new URL('/tonconnect-manifest.json', APP_URL).href;
 export const TELEGRAM_APP_URL = import.meta.env.VITE_TELEGRAM_APP_URL;
 export const TELEGRAM_BOT_URL = import.meta.env.VITE_TELEGRAM_BOT_URL;
+export const TELEGRAM_BOT_ID = import.meta.env.VITE_TELEGRAM_BOT_ID;
 export const DEFAULT_START_PARAM = import.meta.env.VITE_DEFAULT_START_PARAM || undefined;
 
 export const EVENT_APP_ID = import.meta.env.VITE_EVENT_APP_ID;
