6066fb9451193ffde43c659ab3028ff0686537ba
antonmazhuto <anton.mazhuto@cere.io>
2025-01-12 05:09:04 +0300

    Improved loader

--- packages/ui/src/components/Loader/Loader.tsx
+++ packages/ui/src/components/Loader/Loader.tsx
@@ -1,8 +1,8 @@
 import { Spinner, SpinnerProps, Text } from '../../index.ts';
 import './Loader.css';
 
-export const Loader = ({ size = 's' }: { size: SpinnerProps['size'] }) => (
-  <div className="loader" style={{}}>
+export const Loader = ({ size = 's', style = {} }: { size: SpinnerProps['size']; style?: React.CSSProperties }) => (
+  <div className="loader" style={style}>
     <Spinner size={size} />
     <Text>This is taking a bit longer than expected</Text>
   </div>
