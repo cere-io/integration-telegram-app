e7e53240a71878ea7dc663a7b24e29d2942a218f
antonmazhuto <anton.mazhuto@cere.io>
2025-02-06 14:55:32 +0300

    Show waiting text in loader after 3 sec

--- packages/ui/src/components/Loader/Loader.tsx
+++ packages/ui/src/components/Loader/Loader.tsx
@@ -1,9 +1,19 @@
 import { Spinner, SpinnerProps, Text } from '../../index.ts';
 import './Loader.css';
+import { useEffect, useState } from 'react';
 
-export const Loader = ({ size = 's', style = {} }: { size: SpinnerProps['size']; style?: React.CSSProperties }) => (
-  <div className="loader" style={style}>
-    <Spinner size={size} />
-    <Text>This is taking a bit longer than expected</Text>
-  </div>
-);
+export const Loader = ({ size = 's', style = {} }: { size: SpinnerProps['size']; style?: React.CSSProperties }) => {
+  const [showText, setShowText] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setShowText(true), 3000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <div className="loader" style={style}>
+      <Spinner size={size} />
+      {showText && <Text>This is taking a bit longer than expected</Text>}
+    </div>
+  );
+};
