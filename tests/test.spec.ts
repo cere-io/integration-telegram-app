3a3327f08b069cb88c3c7c63d76b5172047d4aec
antonmazhuto <anton.mazhuto@cere.io>
2025-02-28 02:55:48 +0300

    Added e2e workflow for stage

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -12,22 +12,53 @@ test.describe('Performance testing for screens', () => {
 
   test('Open active quests screen', async ({ page }) => {
     const start = Date.now();
-    await page.goto('https://telegram-viewer-app.stage.cere.io/campaignId=115');
+    await page.goto('https://telegram-viewer-app.stage.cere.io/?campaignId=117');
 
-    // await page.locator('.tgui-bca5056bf34297b0').click();
-    // await page.locator('.welcom-cta-text').click();
+    await page.locator('.tgui-bca5056bf34297b0').click();
+    await page.locator('.welcom-cta-text').click();
 
-    await page.locator('#sign_in').click();
-    await page.locator('[name="login"]').fill(userName);
-    await page.locator(':r1:').click();
-    await page.locator('#otp').fill(otp);
-    await page.locator(':r3:').click();
-
-    const questTitle = await page.locator('.t1uqjrzu').innerText();
-    expect(questTitle).toBe('Complete Quests to Earn!');
-
-    const questTab = await page.locator('.tgui-e6658d0b8927f95e').innerText();
-    expect(questTab).toBe('Active Quests');
+    await page
+      .locator('#torusIframe')
+      .contentFrame()
+      .locator('iframe[title="Embedded browser"]')
+      .contentFrame()
+      .getByRole('button', { name: 'I already have a wallet' })
+      .click();
+    await page
+      .locator('#torusIframe')
+      .contentFrame()
+      .locator('iframe[title="Embedded browser"]')
+      .contentFrame()
+      .getByRole('textbox', { name: 'Email' })
+      .click();
+    await page
+      .locator('#torusIframe')
+      .contentFrame()
+      .locator('iframe[title="Embedded browser"]')
+      .contentFrame()
+      .getByRole('textbox', { name: 'Email' })
+      .fill(userName);
+    await page
+      .locator('#torusIframe')
+      .contentFrame()
+      .locator('iframe[title="Embedded browser"]')
+      .contentFrame()
+      .getByRole('button', { name: 'Sign In' })
+      .click();
+    await page
+      .locator('#torusIframe')
+      .contentFrame()
+      .locator('iframe[title="Embedded browser"]')
+      .contentFrame()
+      .getByRole('textbox', { name: 'OTP input' })
+      .fill(otp);
+    await page
+      .locator('#torusIframe')
+      .contentFrame()
+      .locator('iframe[title="Embedded browser"]')
+      .contentFrame()
+      .getByRole('button', { name: 'Verify' })
+      .click();
 
     const end = Date.now();
     const timeTaken = end - start;
@@ -38,32 +69,56 @@ test.describe('Performance testing for screens', () => {
 
   test('Open leaderboard screen', async ({ page }) => {
     const start = Date.now();
-    await page.goto('https://telegram-viewer-app.stage.cere.io/campaignId=115');
+    await page.goto('https://telegram-viewer-app.stage.cere.io/?campaignId=117');
 
-    // await page.locator('.tgui-bca5056bf34297b0').click();
-    // await page.locator('.welcom-cta-text').click();
+    await page.locator('.tgui-bca5056bf34297b0').click();
+    await page.locator('.welcom-cta-text').click();
 
-    await page.locator('#sign_in').click();
-    await page.locator('[name="login"]').fill(userName);
-    await page.locator(':r1:').click();
-    await page.locator('#otp').fill(otp);
-    await page.locator(':r3:').click();
+    await page
+      .locator('#torusIframe')
+      .contentFrame()
+      .locator('iframe[title="Embedded browser"]')
+      .contentFrame()
+      .getByRole('button', { name: 'I already have a wallet' })
+      .click();
+    await page
+      .locator('#torusIframe')
+      .contentFrame()
+      .locator('iframe[title="Embedded browser"]')
+      .contentFrame()
+      .getByRole('textbox', { name: 'Email' })
+      .click();
+    await page
+      .locator('#torusIframe')
+      .contentFrame()
+      .locator('iframe[title="Embedded browser"]')
+      .contentFrame()
+      .getByRole('textbox', { name: 'Email' })
+      .fill(userName);
+    await page
+      .locator('#torusIframe')
+      .contentFrame()
+      .locator('iframe[title="Embedded browser"]')
+      .contentFrame()
+      .getByRole('button', { name: 'Sign In' })
+      .click();
+    await page
+      .locator('#torusIframe')
+      .contentFrame()
+      .locator('iframe[title="Embedded browser"]')
+      .contentFrame()
+      .getByRole('textbox', { name: 'OTP input' })
+      .fill(otp);
+    await page
+      .locator('#torusIframe')
+      .contentFrame()
+      .locator('iframe[title="Embedded browser"]')
+      .contentFrame()
+      .getByRole('button', { name: 'Verify' })
+      .click();
 
-    const questTitle = await page.locator('.t1uqjrzu').innerText();
-    expect(questTitle).toBe('Complete Quests to Earn!');
-
-    await page.locator('/html/body/div[1]/div/div/div[2]/button[2]').click();
-
-    const leaderboardTab = await page.locator('.tgui-e6658d0b8927f95e').innerText();
-    expect(leaderboardTab).toBe('Leaderboard');
-
-    const leaderboardTitle = await page.locator('.jss1').innerText();
-    expect(leaderboardTitle).toBe('Leaderboard');
-
-    await page.locator('.l1shii3t').click();
-
-    const leaderboardResult = await page.locator('.p1kqqlhg').innerText();
-    expect(leaderboardResult).toBe('1 out of 3 tasks completed – Could do better');
+    await page.getByRole('button', { name: 'Leaderboard' }).click();
+    await page.locator('iframe[title="Leaderboard"]').contentFrame().getByRole('img').click();
 
     const end = Date.now();
     const timeTaken = end - start;
@@ -74,24 +129,55 @@ test.describe('Performance testing for screens', () => {
 
   test('Open library screen', async ({ page }) => {
     const start = Date.now();
-    await page.goto('https://telegram-viewer-app.stage.cere.io/campaignId=115');
+    await page.goto('https://telegram-viewer-app.stage.cere.io/?campaignId=117');
 
-    // await page.locator('.tgui-bca5056bf34297b0').click();
-    // await page.locator('.welcom-cta-text').click();
+    await page.locator('.tgui-bca5056bf34297b0').click();
+    await page.locator('.welcom-cta-text').click();
 
-    await page.locator('#sign_in').click();
-    await page.locator('[name="login"]').fill(userName);
-    await page.locator(':r1:').click();
-    await page.locator('#otp').fill(otp);
-    await page.locator(':r3:').click();
+    await page
+      .locator('#torusIframe')
+      .contentFrame()
+      .locator('iframe[title="Embedded browser"]')
+      .contentFrame()
+      .getByRole('button', { name: 'I already have a wallet' })
+      .click();
+    await page
+      .locator('#torusIframe')
+      .contentFrame()
+      .locator('iframe[title="Embedded browser"]')
+      .contentFrame()
+      .getByRole('textbox', { name: 'Email' })
+      .click();
+    await page
+      .locator('#torusIframe')
+      .contentFrame()
+      .locator('iframe[title="Embedded browser"]')
+      .contentFrame()
+      .getByRole('textbox', { name: 'Email' })
+      .fill(userName);
+    await page
+      .locator('#torusIframe')
+      .contentFrame()
+      .locator('iframe[title="Embedded browser"]')
+      .contentFrame()
+      .getByRole('button', { name: 'Sign In' })
+      .click();
+    await page
+      .locator('#torusIframe')
+      .contentFrame()
+      .locator('iframe[title="Embedded browser"]')
+      .contentFrame()
+      .getByRole('textbox', { name: 'OTP input' })
+      .fill(otp);
+    await page
+      .locator('#torusIframe')
+      .contentFrame()
+      .locator('iframe[title="Embedded browser"]')
+      .contentFrame()
+      .getByRole('button', { name: 'Verify' })
+      .click();
 
-    const questTitle = await page.locator('.t1uqjrzu').innerText();
-    expect(questTitle).toBe('Complete Quests to Earn!');
-
-    await page.locator('/html/body/div[1]/div/div/div[2]/button[3]').click();
-
-    const libraryTitle = await page.locator('.tgui-72c2a480384c4fb1').innerText();
-    expect(libraryTitle).toBe('Library');
+    await page.getByRole('button', { name: 'Library' }).click();
 
     const end = Date.now();
     const timeTaken = end - start;
