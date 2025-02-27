968e5525178bf7f87d8ef08d38ebd38a130136f6
Anton Mazhuto <125869076+mazhutoanton@users.noreply.github.com>
2025-02-28 02:57:38 +0300

    Hide torus iframe only in telegram (#107)

--- apps/viewer/src/hooks/use-force-hide-torus-iframe.tsx
+++ apps/viewer/src/hooks/use-force-hide-torus-iframe.tsx
@@ -2,22 +2,29 @@ import { useEffect } from 'react';
 
 export const useForceHideTorusIframe = () => {
   useEffect(() => {
-    const observer = new MutationObserver(() => {
-      const torusIframe = document.getElementById('torusIframe');
-      if (torusIframe) {
-        torusIframe.style.width = '0px';
-        torusIframe.style.height = '0px';
-        torusIframe.style.position = 'absolute';
-        torusIframe.style.visibility = 'hidden';
-        torusIframe.style.display = 'none';
-        torusIframe.style.zIndex = '-2';
-      }
-    });
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    const initDataRaw = window.Telegram?.WebApp?.initData || '';
+    const isInTelegram = initDataRaw !== '';
+    console.log(`In Telegram: ${isInTelegram}`);
+    if (isInTelegram) {
+      const observer = new MutationObserver(() => {
+        const torusIframe = document.getElementById('torusIframe');
+        if (torusIframe) {
+          torusIframe.style.width = '0px';
+          torusIframe.style.height = '0px';
+          torusIframe.style.position = 'absolute';
+          torusIframe.style.visibility = 'hidden';
+          torusIframe.style.display = 'none';
+          torusIframe.style.zIndex = '-2';
+        }
+      });
 
-    const config = { attributes: true, childList: true, subtree: true };
+      const config = { attributes: true, childList: true, subtree: true };
 
-    observer.observe(document.body, config);
+      observer.observe(document.body, config);
 
-    return () => observer.disconnect();
+      return () => observer.disconnect();
+    }
   }, []);
 };
