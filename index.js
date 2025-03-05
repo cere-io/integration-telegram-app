f27606ba979ec5ccdb18e06b56826d0799eb1459
antonmazhuto <anton.mazhuto@cere.io>
2025-03-05 13:32:43 +0300

    Run playwright tests in Lambda functions

--- index.js
+++ index.js
@@ -2,15 +2,13 @@ import { exec } from 'child_process';
 import fs from 'fs';
 
 export const lambdaHandler = async () => {
-  const resultsDir = '/tmp/test-results'; // Директория для результатов тестов
-  const testsDir = '/var/task/tests'; // Директория, где находятся тесты Playwright
+  const resultsDir = '/tmp/test-results';
+  const testsDir = '/var/task/tests';
 
-  // Проверяем, существует ли директория для результатов тестов
   if (!fs.existsSync(resultsDir)) {
-    fs.mkdirSync(resultsDir, { recursive: true }); // Создаем директорию
+    fs.mkdirSync(resultsDir, { recursive: true });
   }
 
-  // Проверяем, существует ли директория с тестами
   if (!fs.existsSync(testsDir)) {
     return { statusCode: 500, body: 'Tests directory not found at: ' + testsDir };
   }
@@ -18,21 +16,17 @@ export const lambdaHandler = async () => {
   console.log('Starting Playwright tests...');
 
   return new Promise((resolve, reject) => {
-    exec(
-      'npx playwright test --reporter=html --output=' + resultsDir, // Указываем путь к результатам
-      { cwd: testsDir }, // Указываем директорию с тестами
-      (error, stdout, stderr) => {
-        if (error) {
-          console.error('Error during Playwright test execution:', stderr);
-          reject({ statusCode: 500, body: `Error: ${stderr}` });
-        } else {
-          console.log('Test execution result:', stdout);
-          resolve({
-            statusCode: 200,
-            body: `Tests executed successfully:\n${stdout}`,
-          });
-        }
-      },
-    );
+    exec('npx playwright test --reporter=html --output=' + resultsDir, { cwd: testsDir }, (error, stdout, stderr) => {
+      if (error) {
+        console.error('Error during Playwright test execution:', stderr);
+        reject({ statusCode: 500, body: `Error: ${stderr}` });
+      } else {
+        console.log('Test execution result:', stdout);
+        resolve({
+          statusCode: 200,
+          body: `Tests executed successfully:\n${stdout}`,
+        });
+      }
+    });
   });
 };
