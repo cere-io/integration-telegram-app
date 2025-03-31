5be3b0e3c3b0557ba0c6ea8c08e902b538c36818
antonmazhuto <anton.mazhuto@cere.io>
2025-03-31 16:32:24 +0300

    Fix text style

--- apps/viewer/src/screens/Media/Media.tsx
+++ apps/viewer/src/screens/Media/Media.tsx
@@ -6,7 +6,6 @@ import { VideoPlayer } from '../../components';
 import { EngagementEventData, Video } from '../../types';
 import { ENGAGEMENT_TIMEOUT_DURATION } from '../../constants.ts';
 import { useData } from '~/providers';
-import { useThemeParams } from '@vkruglikov/react-telegram-web-app';
 
 export type MediaTypeProps = {
   videoUrl?: string;
@@ -14,7 +13,6 @@ export type MediaTypeProps = {
 
 export const Media = ({ videoUrl }: MediaTypeProps) => {
   const { questData, updateData, updateQuestStatus } = useData();
-  const [theme] = useThemeParams();
   const [videos, setVideos] = useState<Video[]>(questData?.[0].quests.videoTasks || []);
   const [currentVideo, setCurrentVideo] = useState<Video>();
   const [pendingUpdates, setPendingUpdates] = useState<Partial<Video>[]>([]);
@@ -109,7 +107,7 @@ export const Media = ({ videoUrl }: MediaTypeProps) => {
 
   return (
     <div style={{ paddingBottom: 65 }}>
-      <Title weight="2" style={{ marginLeft: 16, marginTop: 16 }}>
+      <Title weight="2" style={{ marginLeft: 16, marginTop: 16, color: '#333' }}>
         Library
       </Title>
       <Text Component="div" style={{ margin: '16px 16px 0 16px', color: 'rgb(113, 118, 132)' }}>
@@ -135,7 +133,7 @@ export const Media = ({ videoUrl }: MediaTypeProps) => {
             ))
           ) : (
             <div style={{ margin: '16px 16px 0px' }}>
-              <Text style={{ color: theme === 'dark' ? '#ffffff' : '#333' }}>No videos available</Text>
+              <Text style={{ color: '#333' }}>No videos available</Text>
             </div>
           )}
         </MediaList>
