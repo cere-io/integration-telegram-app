1b15dbbe9ae61e3b9ace812c8f7b127312c8ecfc
antonmazhuto <anton.mazhuto@cere.io>
2025-04-09 11:36:46 +0300

    Fixed firsts render of leaderboard

--- apps/viewer/src/components/IframeRenderer/IframeRenderer.tsx
+++ apps/viewer/src/components/IframeRenderer/IframeRenderer.tsx
@@ -27,17 +27,32 @@ function normalizeTemplateData(html: string): any {
 }
 
 function deepEqualIgnoringSeconds(obj1: any, obj2: any): boolean {
-  if (typeof obj1 !== 'object' || typeof obj2 !== 'object' || obj1 === null || obj2 === null) {
+  if (obj1 === obj2) return true;
+
+  if (obj1 == null || obj2 == null) return false;
+
+  if (typeof obj1 !== 'object' || typeof obj2 !== 'object') {
     return obj1 === obj2;
   }
 
-  const keys1 = Object.keys(obj1).filter((key) => !(key === 'seconds' && obj1 === obj1.remainingTime));
-  const keys2 = Object.keys(obj2).filter((key) => !(key === 'seconds' && obj2 === obj2.remainingTime));
+  const keys1 = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
 
   if (keys1.length !== keys2.length) return false;
 
   for (const key of keys1) {
-    if (!deepEqualIgnoringSeconds(obj1[key], obj2[key])) return false;
+    if (key === 'remainingTime' && obj1[key] && obj2[key]) {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { seconds, ...rt1 } = obj1[key];
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { seconds: _, ...rt2 } = obj2[key];
+
+      if (!deepEqualIgnoringSeconds(rt1, rt2)) {
+        return false;
+      }
+    } else if (!Object.prototype.hasOwnProperty.call(obj2, key) || !deepEqualIgnoringSeconds(obj1[key], obj2[key])) {
+      return false;
+    }
   }
 
   return true;
